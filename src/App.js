2248216dc85cd4ebaac70d325e5332cf67cc3abc
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-21 23:14:44 +0300

    POST to the server on the  TransactionForm sumbit functionality implimented

--- src/App.js
+++ src/App.js
@@ -21,7 +21,25 @@ function App() {
   //
   //
   //
-  //****************************************
+  //************************************************************ */
+
+  function PostFormObjectToServer(formObject) {
+    console.log(formObject);
+    setWatchedByEffect(!watchedByEffect);
+    fetch(`http://localhost:3000/transactions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formObject),
+    });
+
+    // .then((res) => res.json())
+    // .then((data) => setWatchedByEffect(!));
+  }
+  //************************************************************ */
+  //
+  //
   //
   function addTransaction() {
     setShowForm(!showForm);
