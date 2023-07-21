9cd90ac3290628144c58c3ddafd3b2721597fb67
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-21 23:17:22 +0300

    built the searchBox component

---
+++ src/Component/SearchBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+function SearchBar({ searchInput, setSearchInput }) {
+  function searchInputHandler(e) {
+    setSearchInput(e.target.value);
+  }
+  return (
+    <div className="search-box">
+      <button className="btn-search">
+        <i className="fas fa-search"></i>
+      </button>
+      <input
+        type="text"
+        className="input-search"
+        onChange={searchInputHandler}
+        placeholder="Type to Search..."
+      />
+      
+    </div>
+  );
+}
+
+export default SearchBar;
